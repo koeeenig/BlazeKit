943afe6d64e917764f5facd26f2ef8aa0bbaef00
koeeenig <andreas.koenig@icarus-consult.de>
2023-11-21 13:06:02 +0100

    💚 Define ENV outsite of script

--- install.sh
+++ install.sh
@@ -3,6 +3,3 @@ yum -y install wget tar gzip libicu
 wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
 chmod +x ./dotnet-install.sh
 ./dotnet-install.sh --version latest
-export DOTNET_ROOT=$HOME/.dotnet
-export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
-dotnet --version
