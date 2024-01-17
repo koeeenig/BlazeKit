5eeba0f8d237036e8e6a0db287009b5b3acd977c
koeeenig <andreas.koenig@icarus-consult.de>
2024-01-17 19:47:56 +0100

    🚧 use ./

--- ssg.sh
+++ ssg.sh
@@ -3,4 +3,4 @@ cd ./src/BlazeKit.Website
 /vercel/.dotnet/dotnet publish -o .blazekit/build/tmp
 /vercel/.dotnet/dotnet run ssg
 mkdir -p ../../.vercel/output/static
-rsync -av .blazekit/build/ssg/ ../../.vercel/output/static/
+rsync -av ./.blazekit/build/ssg/ ./../../.vercel/output/static/
